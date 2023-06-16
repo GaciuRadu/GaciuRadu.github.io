eedd17de185611f63430edac9670c16c3f8f73be
GaciuRadu <gaciuradu@gmail.com>
2023-06-16 18:33:45 +0300

    update index.js

--- index.js
+++ index.js
@@ -3,14 +3,24 @@ function showHome() {
   page.style.display = "block";
 }
 showHome();
+
 function showSkills() {
   var page = document.getElementById("skills");
   console.info(page);
   page.style.display = "block";
+  console.warn("hei te exectuti");
 }
 
 function showLanguages() {
   var page = document.getElementById("languages");
   console.info(page);
   page.style.display = "block";
+  console.warn("hei te exectuti");
+}
+
+function showProjects() {
+  var page = document.getElementById("projects");
+  console.info(page);
+  page.style.display = "block";
+  console.warn("hei te exectuti");
 }

12eb66e9c5dbf79ddfe925b4099f683c5a6e25c8
GaciuRadu <gaciuradu@gmail.com>
2023-06-16 17:38:36 +0300

    Verify

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ function showHome() {
   var page = document.getElementById("home");
   page.style.display = "block";
 }
-
+showHome();
 function showSkills() {
   var page = document.getElementById("skills");
   page.style.display = "block";
 }
 
-showHome();
-
-function showlanguages() {
+function showLanguages() {
   var page = document.getElementById("languages");
+  page.style.display.getElementById("block");
+}
+
+function showContact() {
+  var page = document.getElementById("??????????");
   page.style.display.getElementById("block");
 }

a6e8c223ee659c15ed0053695e29262028227201
GaciuRadu <gaciuradu@gmail.com>
2023-06-16 20:01:10 +0300

    remouve all  showX function and use only showPage

--- index.js
+++ index.js
@@ -4,36 +4,11 @@ function hideAllPages() {
   document.getElementById("languages").style.display = "none";
   document.getElementById("projects").style.display = "none";
 }
-
-function showLanguages() {
-  showPage("languages");
-  // hideAllPages();
-  // var page = document.getElementById("home");
-  // page.style.display = "none";
-
-  // var page = document.getElementById("languages");
-  // console.info(page);
-  // page.style.display = "block";
-  // console.warn("hei te exectuti");
-}
-
-function showProjects() {
-  showPage("Projects");
-  // hideAllPages();
-  // var page = document.getElementById("home");
-  // page.style.display = "none";
-
-  // var page = document.getElementById("projects");
-  // console.info(page);
-  // page.style.display = "block";
-  // console.warn("hei te exectuti");
-}
-
 function showPage(id) {
   hideAllPages();
   var page = document.getElementById(id);
-  console.info("show", page);
+  console.info("show", id, page);
   page.style.display = "block";
 }
 
-showPage("Home");
+showPage("home");

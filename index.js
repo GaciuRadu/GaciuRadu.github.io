d114670004a55346df035b2e0a4d366a66e3bbdc
GaciuRadu <gaciuradu@gmail.com>
2023-06-16 20:39:28 +0300

    Improve hidePages using hide element function

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-function hideAllPages() {
-  document.getElementById("home").style.display = "none";
-  document.getElementById("skills").style.display = "none";
-  document.getElementById("languages").style.display = "none";
-  document.getElementById("projects").style.display = "none";
+function hide(id) {
+  console.info("hide %o element", id);
+  document.getElementById(id).style.display = "none";
 }
+
+function hideAllPages() {
+  hide("home");
+  hide("skills");
+  hide("languages");
+  hide("projects");
+}
+
 function showPage(id) {
   hideAllPages();
   var page = document.getElementById(id);
-  console.info("show", id, page);
+  console.info("show %o ..", id, page);
   page.style.display = "block";
 }
 

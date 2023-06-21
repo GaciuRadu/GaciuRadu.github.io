6a71ef0bd4224431f3a76d69e9231921d9f8570f
GaciuRadu <gaciuradu@gmail.com>
2023-06-21 13:59:11 +0300

    improve console.info for vizibility

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ var activePage = "home";
 // utilities function
 
 function hide(id) {
-  console.info("hide %o element", id);
+  console.info("hide %o  element hide", id);
   document.getElementById(id).style.display = "none";
 }
 
 function showPage(id) {
   hide(activePage);
   var page = document.getElementById(id);
-  console.info("show %o ..", id, page);
+  console.info("show %o  element showPage..", id, page);
   page.style.display = "block";
   activePage = id;
 }
